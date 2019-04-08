8a2f9842ae05a9a834324f832aab82acf6e1b645
kuan <woaini3900098@qq.com>
2019-04-09 00:19:16 +0800

    新增文件上传

--- core/fast/Upload.php
+++ core/fast/Upload.php
@@ -44,18 +44,73 @@ use core\fast\Conf;
  	public function setOption($key,$val){
 		$this->$key = $val;
  	}
+ 	//获取原文件名
+ 	public function getName(){
+ 		return $this->upload_name;
+ 	}
  	//文件上传方法
  	public function upload($fileName='file'){
  		//获取上传信息
 		if($this->setFiles($fileName)){
+			if($this->checkFilePath()){
 
+			}
 		}
+ 	}
+ 	//检查上传目录
+ 	public function checkFilePath(){
+ 		//判断是否设置了存放路径
+ 		if(empty($this->upload_target_dir)){
+			$this->setOption('upload_error',-5);
+			return false;
+ 		}
  	}
  	//获取文件上传信息
  	public function setFiles($fileName){
 		$this->upload_name = $_FILES[$fileName]["name"]; //取得上传文件名
+ 		$this->upload_error = $_FILES[$fileName]["error"];
+ 		if($this->upload_error > 0){
+			return false;
+ 		}
 		$this->upload_filetype = $_FILES[$fileName]["type"];
 		$this->upload_tmp_name = $_FILES[$fileName]["tmp_name"];
 		$this->upload_file_size = $_FILES["file"]["size"];
+ 	}
+ 	//设置错误信息
+ 	public function getError(){
+ 		$str = '上传文件时<font color="red">'.$this->upload_name.'</font>出错:'
+ 		switch ($this->upload_error) {
+            case 4:
+                $str .= "没有文件被上传";
+                break;
+            case 3:
+                $str .= "文件只有部分被上传";
+                break;
+            case 2:
+                $str .= "上传文件的大小超过了HTML表单中MAX_FILE_SIZE选项指定的值";
+                break;
+            case 1:
+                $str .= "上传的文件超过了php.ini中upload_max_filesize选项限制的值";
+                break;
+            case -1:
+                $str .= "未允许类型";
+                break;
+            case -2:
+                $str .= '文件过大,上传的文件不能超过'.$this->allow_uploaded_maxsize.'个字节';
+                break;
+            case -3:
+                $str .= "上传失败";
+                break;
+            case -4:
+                $str .= "建立存放上传文件目录失败，请重新指定上传目录";
+                break;
+            case -5:
+                $str .= "必须指定上传文件的路径";
+                break;
+            default:
+                $str .= "未知错误";
+        }
+        return $str . '<br>';
+ 		}
  	}
  }
